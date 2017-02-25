4b1f2592ad8652efbd5792b0cbe44bdb6500c710
Thomas Höglund <thomas.hoeglund@gmail.com>
2017-02-25 11:18:29 +0200

    Created the repo
    BBQ spit controlled by an Arduino

---
+++ ArduinoRotisserieController.ino
@@ -0,0 +1,24 @@
+/*
+ * Created by Thomas Höglund
+ * Code by Norman Medi
+ * Mechanics by Milan Adamovic
+ * 
+ * Updated 25.2.2017
+ * 
+ * Description of algorithm
+ * 
+ * 
+ * Description of mechanics
+ * 
+ * 
+ */
+
+void setup() {
+  
+  
+}
+
+void loop() {
+  
+  
+}
